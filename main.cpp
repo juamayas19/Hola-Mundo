3aba4eac0c7e51e5b06c8f3f88303f949d116fd0
Estudiante <Estudiante@10.203.1.243>
2018-08-14 08:24:29 -0500

    There is the Hola Mundo's classic

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Estudiante
+ *
+ * Created on 14 de agosto de 2018, 08:19 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+        std::cout<<"Hola mundo";
+    return 0;
+}
+
